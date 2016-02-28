bd3934db68568898b524a0cb03abdbf710445ee5
yabhis <abhishek291093@gmail.com>
2016-02-28 17:52:02 +0530

    nav button full functioning with animation

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,15 +1,20 @@
 jQuery(document).ready(function($) {
-	$('.nav-button').click(function(event) {
-		/* Act on the event */
-		$('.mobile-nav').toggle();
-		$('.nav-button-icon').toggle(function() {
-			/* Stuff to do every *odd* time the element is clicked */
-			$('nav-button').html('<i class="nav-button-icon fa fa-bars fa-2x"></i>');
-		}, function() {
-			/* Stuff to do every *even* time the element is clicked */
-			$('nav-button').html('<i class="nav-button-icon fa fa-times fa-2x"></i>');
+    // $('.nav-button').click(function(event) {
+    //     /* Act on the event */
+    //     // $('.mobile-nav').toggle();
+    //     $('.nav-button-icon').toggle(function() {
+    //         /* Stuff to do every *odd* time the element is clicked */
+    //         $('nav-button').html('<i class="nav-button-icon fa fa-bars fa-2x"></i>');
+    //     }, function() {
+    //         /* Stuff to do every *even* time the element is clicked */
+    //         $('nav-button').html('<i class="nav-button-icon fa fa-times fa-2x"></i>');
 
-		});
-		;
-	});
+    //     });
+    // });
+
+    $('#menu-icon-trigger').click(function(e) {
+        e.preventDefault();
+         $('.mobile-nav').toggle();
+        $('#menu-icon-wrapper').toggleClass('open');
+    });
 });
